483ed17532fda814b079d4803e517ba71b7f4750
B <public88878878887@hotmail.com>
2016-04-24 17:41:42 +0000

    don't test the framework

--- test/unit/ngActionCable.spec.js
+++ test/unit/ngActionCable.spec.js
@@ -14,16 +14,4 @@ describe('ngActionCable', function(){
       expect(ngActionCable.hasModule('ngWebSocket')).toBeTruthy;
     });
   });
-  describe('dependent', function(){
-    var mySampleApp;
-    beforeEach(function(){
-      mySampleApp= angular.module('mySampleApp', ['ngActionCable']);
-      mySampleApp.hasModule= function(module) {
-        return mySampleApp.requires.indexOf(module) >= 0;
-      };
-    });
-    it('ngActionCable loaded', function(){
-      expect(mySampleApp.hasModule('ngActionCable')).toBeTruthy;
-    });
-  });
 });
